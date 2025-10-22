dc7030d8d557b03927a571ffc4958a2144191520
marklvov <marklvov@altmail.kr>
2025-10-23 00:46:32 +0200

    auth.ts db fix

--- auth.ts
+++ auth.ts
@@ -30,7 +30,7 @@ export const {
     async signIn({ user, account }) {
       if (account?.provider !== 'credentials') return true
 
-      const existingUser = await getUserById(user.id)
+      const existingUser = await getUserById(user.id!)
 
       if (!existingUser?.emailVerified) return false
 
@@ -60,8 +60,8 @@ export const {
       }
 
       if (session.user) {
-        session.user.name = token.name;
-        session.user.email = token.email;
+        session.user.name = token.name || '';
+        session.user.email = token.email || '';
         session.user.isOAuth = token.isOAuth as boolean;
       }
 
