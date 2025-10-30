76ab4e955eaa385cb591f270630c1252a98966ac
marklvov <marklvov@altmail.kr>
2025-10-31 00:41:56 +0100

    next config js modified

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
 const createNextIntlPlugin = require('next-intl/plugin');
-
 const withNextIntl = createNextIntlPlugin('./lib/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['bcryptjs'],
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
-    domains: [
-      "res.cloudinary.com"
+    domains: ["res.cloudinary.com"],
+    unoptimized: true 
+  },
+  skipTrailingSlashRedirect: true,
+  async redirects() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://abc-chi-green.vercel.app/api/:path*',
+        permanent: false,
+      },
     ]
   }
 }
